b4dc7065fe070ee4399ee1020a4573e5de12e025
cjkarinen20 <cjkarinen20@gmail.com>
2026-03-16 11:51:48 -0400

    Basic Dynamic Routing
    Added basic dynamic routing functionality to the
    'Hello World' program. The program can now
    display a custom greeting message with the user's
    name in it.

--- flask-project/apps/hello-world/app.py
+++ flask-project/apps/hello-world/app.py
@@ -6,3 +6,10 @@ app = Flask(__name__)
 
 def hello_world():
     return 'Hello World!'
+
+#---Dynamic-Routing---
+@app.route('/user/<name>')
+def user(name):
+    personal = f'<h1>Hello, {name}!</h1>'
+    template = '<p>Change the name in the <em> browser address bar</em> and reload the page.</p>'
+    return personal + template

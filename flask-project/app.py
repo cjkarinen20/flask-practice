31f43986ea320559494116c5ef1f46698d1182d9
cjkarinen20 <cjkarinen20@gmail.com>
2026-03-16 14:25:06 -0400

    Task List and Working Submit Button
    -Created a task list screen.
    -Added functionality to the submit button.
    -Added update functionality to the task list.

--- flask-project/app.py
+++ flask-project/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf import FlaskForm
@@ -19,18 +19,38 @@ class Task(db.Model):
     description = db.Column(db.String(200), nullable = True)
     is_complete = db.Column(db.Boolean, default = False)
     
-#---Forms---
+#---Info-Form---
 class TaskForm(FlaskForm):
     title = StringField('Title', validators=[InputRequired(), Length(min=1, max=100)])
     description = TextAreaField('Description', validators=[Length(max=200)])
     is_complete = BooleanField('Completed')
     submit = SubmitField('Submit')
+    
 @app.route('/hello/task', methods=['GET', 'POST'])
 def task():
     form = TaskForm()
-    if form.validate_on_submit():
+    if form.validate_on_submit(): # Submit Button Logic
+        new_task = Task(
+            title = form.title.data,
+            description = form.description.data,
+            is_complete = form.is_complete.data
+        )
+        db.session.add(new_task)
+        db.session.commit()
         return redirect(url_for('hello_world'))
     return render_template('task.html', form = form)
+
+@app.route('/task/list')
+def display_tasks():
+    tasks = Task.query.all()
+    return render_template('task_list.html', tasks=tasks)
+
+@app.route('/task/update_status/<int:task_id>', methods = ["POST"])
+def update_task_status(task_id):
+    task = Task.query.get_or_404(task_id)
+    task.is_complete = 'is_complete' in request.form
+    db.session.commit()
+    return redirect(url_for('display_tasks'))
 
 @app.route('/')
 def hello_world():

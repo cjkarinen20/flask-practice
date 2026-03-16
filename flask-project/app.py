452d0a0973be6d8c82e621672c0bc4b219038c74
cjkarinen20 <cjkarinen20@gmail.com>
2026-03-16 13:38:17 -0400

    Task Forms
    -Added a form page to submit a message with a
    title and description.
    -The submissions are now saved to the database.

--- flask-project/app.py
+++ flask-project/app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField, BooleanField
+from wtforms.validators import InputRequired, Length
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hello-world.db'
@@ -8,15 +12,27 @@ app.config['SECRET_KEY'] = '96c2dda30278499a85e2eb8a0bbbbed9c84e79ad52114b44'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-#---Database---
+#---Databases---
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable = False)
     description = db.Column(db.String(200), nullable = True)
     is_complete = db.Column(db.Boolean, default = False)
+    
+#---Forms---
+class TaskForm(FlaskForm):
+    title = StringField('Title', validators=[InputRequired(), Length(min=1, max=100)])
+    description = TextAreaField('Description', validators=[Length(max=200)])
+    is_complete = BooleanField('Completed')
+    submit = SubmitField('Submit')
+@app.route('/hello/task', methods=['GET', 'POST'])
+def task():
+    form = TaskForm()
+    if form.validate_on_submit():
+        return redirect(url_for('hello_world'))
+    return render_template('task.html', form = form)
 
 @app.route('/')
-
 def hello_world():
     return 'Hello World!'
 

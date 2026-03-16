357f33fdc66ef9eb389d851ddc6160c8332d456d
cjkarinen20 <cjkarinen20@gmail.com>
2026-03-16 13:02:59 -0400

    Database Integration
    -Added SQLAlchemy to the project.
    -Created database entries for user information.
    -Made first flask db migration.

--- flask-project/app.py
+++ flask-project/app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hello-world.db'
+app.config['SECRET_KEY'] = '96c2dda30278499a85e2eb8a0bbbbed9c84e79ad52114b44'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+#---Database---
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable = False)
+    description = db.Column(db.String(200), nullable = True)
+    is_complete = db.Column(db.Boolean, default = False)
 
 @app.route('/')
 

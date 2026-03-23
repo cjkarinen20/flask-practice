e775ebcf3e7915ca841b49b62935e1a11349b452
cjkarinen20 <cjkarinen20@gmail.com>
2026-03-23 18:44:04 -0400

    Moved API Key to .env file
    -Installed dotenv into the project environment.
    -Added API key to .env file.
    -Imported API key from env file using os.getenv.

--- flask-project/myapp/__init__.py
+++ flask-project/myapp/__init__.py
@@ -1,11 +1,12 @@
+import os
 from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hello-world.db'
-app.config['SECRET_KEY'] = '96c2dda30278499a85e2eb8a0bbbbed9c84e79ad52114b44'
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
